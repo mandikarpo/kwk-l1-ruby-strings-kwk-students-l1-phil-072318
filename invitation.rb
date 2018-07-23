04fc2f7ba9e6362410bbb73ce7807a0786faa4af
Mandi Karpo <mandi.karpo@gmail.com>
2018-07-23 20:21:53 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -21,4 +21,10 @@ puts "Who is hosting the party?"
 
 host_name = gets.chomp 
 
+puts "Dear #{guest_name},"
+
+puts "You are cordially invited to the #{party_name} on #{date} at #{time}.Please RSVP no later than October 30." 
+
+puts "Sincerely, #{host_name}."
+
 
